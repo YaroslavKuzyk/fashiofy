66837f643c5ac514b14abab53d3c12506363fb0f
YaroslavKuzyk <43239101+YaroslavKuzyk@users.noreply.github.com>
2023-08-22 17:40:24 +0000

    Deploying to gh-pages from @ YaroslavKuzyk/fashiofy@10914178b6b32800544202c541a93355f46c7991 🚀

--- server/_nuxt/default-styles-1.mjs-c1d17912.js
+++
@@ -1,5 +0,0 @@
-const Logo_vue_vue_type_style_index_0_scoped_3b9be177_lang = ".logo[data-v-3b9be177]{align-items:center;display:inline-flex;flex-direction:column;justify-content:center}.logo__text[data-v-3b9be177]{color:var(--primary);font-family:AbhayaLibre;font-size:23px;font-weight:400;line-height:normal;margin-top:6px}";
-export {
-  Logo_vue_vue_type_style_index_0_scoped_3b9be177_lang as default
-};
-//# sourceMappingURL=default-styles-1.mjs-c1d17912.js.map
--- server/_nuxt/index-styles-3.mjs-8a20bc4b.js
+++
@@ -1,5 +0,0 @@
-const Carousel_vue_vue_type_style_index_1_lang = ".carousel__pagination-button--active:after,.carousel__pagination-button:hover:after{background-color:var(--extra-light)}.carousel__icon{fill:var(--extra-light)}.carousel,.carousel__track,.carousel__viewport{height:100%}";
-export {
-  Carousel_vue_vue_type_style_index_1_lang as default
-};
-//# sourceMappingURL=index-styles-3.mjs-8a20bc4b.js.map
