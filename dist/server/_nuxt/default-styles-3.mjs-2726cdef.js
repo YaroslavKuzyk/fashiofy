4b6bb8817718e895f99badcee0bd5929f4a38e0f
YaroslavKuzyk <43239101+YaroslavKuzyk@users.noreply.github.com>
2023-08-23 07:29:20 +0000

    Deploying to gh-pages from @ YaroslavKuzyk/fashiofy@5e990e6a37047ff5359255a727c35bccbaa55a63 🚀

---
+++ dist/server/_nuxt/default-styles-3.mjs-2726cdef.js
@@ -0,0 +1,5 @@
+const SalesBar_vue_vue_type_style_index_0_scoped_253117a4_lang = ".sales-bar[data-v-253117a4]{background:var(--extra-dark);color:var(--extra-light);font-size:var(--font-xxs);font-style:normal;font-weight:500;letter-spacing:-.1px;line-height:18px;padding:6px;position:relative;text-align:center}.sales-bar__close[data-v-253117a4]{cursor:pointer;height:12px;position:absolute;right:6px;top:50%;transform:translateY(-50%);width:12px}";
+export {
+  SalesBar_vue_vue_type_style_index_0_scoped_253117a4_lang as default
+};
+//# sourceMappingURL=default-styles-3.mjs-2726cdef.js.map
